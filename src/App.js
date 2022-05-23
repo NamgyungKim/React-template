e8e0b203014b2e1c0baed7c77ca39b1fb626a4f7
NamgyungKim <namgyung.kim@gmail.com>
2022-05-24 01:55:24 +0900

    🎉 init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import useKeyPress from "./hooks/useKeyPress";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const keyPressed = useKeyPress("a");
+  return <div>key: {keyPressed && "Pressed"}</div>;
+};
 
 export default App;
